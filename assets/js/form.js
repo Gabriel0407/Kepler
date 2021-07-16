15128c80c06b39bf601346dbf38d229d5e872cf2
Gabriel0407 <gabriel.elias0407@gmail.com>
2021-07-16 15:48:31 -0300

    form update

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,15 +5,19 @@ register.addEventListener("click",()=>{
     const container = document.createElement("div");
     const formContainer = document.createElement("div");
     const close = document.createElement("i");
-
+    const form = document.createElement("form");
+    
     container.setAttribute("class","container");
     formContainer.setAttribute("class","formContainer");
     close.setAttribute("class","fas fa-times close-button");
-
+    form.setAttribute("name","contact");
+    form.setAttribute("method","POST");
+    form.setAttribute("data-netlify","true");
+    
     popUp.appendChild(container);
     container.appendChild(formContainer);
     formContainer.appendChild(close);
-
+    formContainer.appendChild(form);
 
     close.addEventListener("click",()=>{
         popUp.removeChild(container);
